3758db1176d27d806d09e5272fdde29f4ac7c654
valeromero7_ <totaromero186@gmail.com>
2023-11-08 23:50:06 -0300

    fallos de js

--- js/carrito.js
+++
@@ -1,3 +0,0 @@
-"use strict"
-alert ("si la cantidad de productos supera los $30000, su envio será gratis")
-let cantProd = prompt("ingrese el nombre de los productos que quiera comprar (bolso, toalla, matera)")
---
+++ js/tienda.js
@@ -0,0 +1,19 @@
+"use strict"
+alert("Con una compra igual o mayor a $20000, obtenes envio gratis")
+let precio = Number(prompt("ingresa el precio del producto que deseas comprar"))
+if (precio == 15000||1000){
+    let agregar = prompt("El precio total de tu producto no te permite obtener envio gratis, desea agregar otro? (escribir 'si' para continuar)")
+    if(agregar != "si"){
+        alert("tu envio tendra un costo de $5000")
+    }else{
+        Number(prompt("ingresa el precio del producto que deseas comprar"))
+    }
+        prompt
+    }
+if(precio = 25000){
+alert("felicidades!! tenes envio gratis en tu producto")}
+
+
+
+
+
