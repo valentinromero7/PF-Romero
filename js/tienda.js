0d33d40e74f11db56c8757641b293581b6de2da0
valeromero7_ <totaromero186@gmail.com>
2023-11-09 17:23:33 -0300

    necesito ayuda con js

--- js/tienda.js
+++ js/tienda.js
@@ -1,19 +1,27 @@
 "use strict"
-alert("Con una compra igual o mayor a $20000, obtenes envio gratis")
-let precio = Number(prompt("ingresa el precio del producto que deseas comprar"))
-if (precio == 15000||1000){
-    let agregar = prompt("El precio total de tu producto no te permite obtener envio gratis, desea agregar otro? (escribir 'si' para continuar)")
-    if(agregar != "si"){
-        alert("tu envio tendra un costo de $5000")
+
+alert("Si el precio de tu compra supera los $20000, obtendras envio GRATIS!!!")
+let valor1 = prompt("escribi el nombre del producto que deseas comprar y calcularemos el precio total de tu compra. (bolso, toalla o matera)")
+let bolso = "bolso"
+let toalla = "toalla"
+let matera = "matera"
+let valorTotal = 0
+let precioBolso = 25000
+let precioToalla = 15000
+let precioMatera = 20000
+let minimoEnv = 20000
+/*
+if (valor1 !== "bolso"||"toalla"||"matera"){
+    alert("El producto ingresado no es valido, por favor ingrese uno de estos tres: bolso, toalla, matera")
+}*/
+if (valor1 == "toalla"){
+    let noCumple = prompt("El total de su compra, no supera los $20000, si desea agregar un producto escriba 'si'")
+    if (noCumple != "si"){
+        alert("Su envio tendra un coste de $5000.")
     }else{
-        Number(prompt("ingresa el precio del producto que deseas comprar"))
+        let valor2 = prompt("escribi el nombre del producto que deseas agregar y calcularemos precio total de tu compra. (bolso, toalla o matera)")
     }
-        prompt
-    }
-if(precio = 25000){
-alert("felicidades!! tenes envio gratis en tu producto")}
-
-
+}
 
 
 
