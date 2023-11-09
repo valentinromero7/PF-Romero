bc11760f1ef2c67a3969eaabe35eb08d8017fb90
valeromero7_ <totaromero186@gmail.com>
2023-11-09 18:02:34 -0300

    añadiendo bandejas a la pagina.

--- js/tienda.js
+++ js/tienda.js
@@ -10,10 +10,10 @@ let precioBolso = 25000
 let precioToalla = 15000
 let precioMatera = 20000
 let minimoEnv = 20000
-/*
+
 if (valor1 !== "bolso"||"toalla"||"matera"){
     alert("El producto ingresado no es valido, por favor ingrese uno de estos tres: bolso, toalla, matera")
-}*/
+}
 if (valor1 == "toalla"){
     let noCumple = prompt("El total de su compra, no supera los $20000, si desea agregar un producto escriba 'si'")
     if (noCumple != "si"){
