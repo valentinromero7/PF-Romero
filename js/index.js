e69b98b218e410e8f631b20eef80a76561f87010
valeromero7_ <totaromero186@gmail.com>
2024-01-06 07:39:37 -0300

    cambiando el main

--- js/index.js
+++ js/index.js
@@ -1,10 +1,1 @@
-"use strict"
-
-alert("Bienvenido a la tienda online de Tesoros Del Campo!")
-
-let nombre=prompt("ingresa tu nombre y apellido aqui debajo")
-function saludar(mensaje){
-    alert(mensaje)
-}
-saludar ("hola" + " " + nombre + "!! Si queres ver mas Javascript, andá a la tienda.")
 
--- js/tienda.js
+++ js/tienda.js
@@ -1,26 +1,3 @@
-    let maximoGastoCliente = Number(prompt("Indique la cantidad máxima de dinero que esté dispuesto a gastar y le indicaremos los productos que sean acordes a su presupuesto"))
-
-while(isNaN(maximoGastoCliente)){
-    maximoGastoCliente = Number(prompt("Por favor indique la cantidad máxima de dinero que esté dispuesto a gastar y le indicaremos los productos que sean acordes a su presupuesto"))
-}
-if(maximoGastoCliente<7499){
-    alert("el monto ingresado es insuficiente para comprar cualquier producto de nuestra tienda.")
-}else if(maximoGastoCliente>=7500 && maximoGastoCliente<=9999){
-    alert("Usted puede adquirir una bandeja pequeña.")
-}else if(maximoGastoCliente>=10000 && maximoGastoCliente<=12499){
-    alert("Usted puede adquirir una bandeja pequeña o mediana.")
-}
-else if(maximoGastoCliente>=12500 && maximoGastoCliente<=14999){
-    alert("Usted puede adquirir una bandeja pequeña mediana o grande.")
-}
-else if(maximoGastoCliente>=15000 && maximoGastoCliente<=24999){
-    alert("Usted puede adquirir varias bandejas o una toalla artesanal.")
-}
-else if(maximoGastoCliente>=25000){
-    alert("Usted puede adquirir cualquier producto de nuestra tienda.")
-}
-let Monto=maximoGastoCliente
-alert ("El monto que usted ingreso es de $" + Monto)
 
 
 
